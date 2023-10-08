e11daca714244ffcb9324fb0797eaf050b9e2762
msashoyeb <msashoyeb@gmail.com>
2023-10-08 10:12:41 +0600

    Version 1.2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Version 1.0 -> Project upload
 
 Version 1.1 -> Scroll reveal added
+
+Version 1.2 -> Google Recaptcha added, Institute picture round croped
--- js/contactMe.js
+++ js/contactMe.js
@@ -1,56 +1,65 @@
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbwMUiff2KMH0IKnamLhJIK25dkDW55D4HNZPvXNQJza_fhAfMrpndjfUig3aZ-cH0PN3w/exec";
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("contact_msg");
-const countdownMsg = document.getElementById("countdown_msg");
+const form = document.getElementById("myForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // Get the reCAPTCHA response token
+  const recaptchaResponse = grecaptcha.getResponse();
 
-  // Disable the submit button while the countdown is active
-  var myButton = document.getElementById("submitBtn");
-  myButton.disabled = true;
+  if (recaptchaResponse.length !== 0) {
+    const scriptURL =
+      "https://script.google.com/macros/s/AKfycbwMUiff2KMH0IKnamLhJIK25dkDW55D4HNZPvXNQJza_fhAfMrpndjfUig3aZ-cH0PN3w/exec";
+    const form = document.forms["submit-to-google-sheet"];
+    const msg = document.getElementById("contact_msg");
+    const countdownMsg = document.getElementById("countdown_msg");
+    // Start the countdown timer
+    let countdown = 2;
+    countdownMsg.textContent = `Message sending in ${countdown} seconds...`;
 
-  // Start the countdown timer
-  let countdown = 2;
-  countdownMsg.textContent = `Redirecting in ${countdown} seconds`;
+    const countdownInterval = setInterval(() => {
+      countdown -= 1;
+      if (countdown <= 1)
+        countdownMsg.textContent = `Message sending in ${countdown} second...`;
+      else
+        countdownMsg.textContent = `Message sending in ${countdown} seconds...`;
+      if (countdown === 0) {
+        clearInterval(countdownInterval);
 
-  const countdownInterval = setInterval(() => {
-    countdown -= 1;
-    countdownMsg.textContent = `Redirecting in ${countdown} seconds`;
-    if (countdown === 0) {
+        // Submit the form
+        fetch(scriptURL, { method: "POST", body: new FormData(form) })
+          .then((response) => {
+            msg.textContent = "Message sent successfully.";
+            msg.style.color = "green";
+            setTimeout(function () {
+              msg.textContent = "";
+            }, 5000);
+
+            // Reset the form and enable the submit button
+            form.reset();
+            grecaptcha.reset();
+            countdownMsg.textContent = "";
+          })
+          .catch((error) => {
+            msg.textContent = "Message failed to send!";
+            msg.style.color = "red";
+            setTimeout(function () {
+              msg.textContent = "";
+            }, 5000);
+            countdownMsg.textContent = "";
+          });
+      }
+    }, 1000);
+
+    // Reset the countdown if the form is reset
+    form.addEventListener("reset", () => {
       clearInterval(countdownInterval);
-
-      // Submit the form
-      fetch(scriptURL, { method: "POST", body: new FormData(form) })
-        .then((response) => {
-          msg.textContent = "Message successfully sent.";
-          msg.style.color = "green";
-          setTimeout(function () {
-            msg.textContent = "";
-          }, 5000);
-
-          // Reset the form and enable the submit button
-          form.reset();
-          myButton.disabled = false;
-          countdownMsg.textContent = "";
-        })
-        .catch((error) => {
-          msg.textContent = "The message could not be sent!";
-          msg.style.color = "red";
-          setTimeout(function () {
-            msg.textContent = "";
-          }, 5000);
-          myButton.disabled = false;
-          countdownMsg.textContent = "";
-        });
-    }
-  }, 1000);
-
-  // Reset the countdown if the form is reset
-  form.addEventListener("reset", () => {
-    clearInterval(countdownInterval);
-    countdownMsg.textContent = "";
-    myButton.disabled = false;
-  });
+      countdownMsg.textContent = "";
+    });
+  } else {
+    const msg = document.getElementById("contact_msg");
+    msg.textContent = "The captcha wasn't entered correctly!";
+    msg.style.color = "red";
+    setTimeout(function () {
+      msg.textContent = "";
+    }, 5000);
+  }
 });
